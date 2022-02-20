1d1d518b35b1cba3c90939b8b95d4cf1cb166bdc
sbarnett <thumdugger@gmail.com>
2022-02-19 16:57:54 -0900

    Update keychain module docstring

--- src/keychain/__init__.py
+++ src/keychain/__init__.py
@@ -1,1 +1,11 @@
+"""
+Module to implement keychain enabled collections.
+
+A keychain is a specialized type of posix path that gives specific meaning to certain path constructs that enable
+naming, path traversal, and globbing.
+
+These keychains can then be utilized by keychain enabled collections for getting and setting values similar, in some
+ways, like one would in a posix filesystem.
+"""
+
 __version__ = '0.0.0'
